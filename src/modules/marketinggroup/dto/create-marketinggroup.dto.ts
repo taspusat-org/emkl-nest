a28a23e7a0fd0040771e0136c9e4d43e52eeb7ed
bshf <bungashafa53@gmail.com>
2025-08-22 13:35:00 +0700

    marketing group crud

---
+++ src/modules/marketinggroup/dto/create-marketinggroup.dto.ts
@@ -0,0 +1,33 @@
+import { dbMssql } from 'src/common/utils/db';
+import { z } from 'zod';
+const checkIfExistsNama = async (marketing_id: number) => {
+  const result = await dbMssql
+    .select('*')
+    .from('marketinggroup')
+    .where('marketing_id', marketing_id)
+    .first();
+
+  return result ? true : false; // Return true jika ada, false jika tidak ada
+};
+
+export const CreateMarketinggroupSchema = z.object({
+  marketing_id: z
+    .number()
+    .int({ message: 'Marketing Wajib Diisi' })
+    .refine(
+      async (value) => {
+        console.log('value', value);
+        const exists = await checkIfExistsNama(value);
+        return !exists; // Validasi jika nama sudah ada
+      },
+      {
+        message: 'Marketing dengan nama ini sudah dibuat group',
+      },
+    ),
+  statusaktif: z
+    .number()
+    .int({ message: 'Status Aktif Wajib Angka' })
+    .nonnegative({ message: 'Status Aktif Tidak Boleh Angka Negatif' }), // Ensure non-negative
+  modifiedby: z.string().nullable().optional(),
+});
+export type CreateMarketinggroupDto = z.infer<typeof CreateMarketinggroupSchema>;
