0f2714bf537ad0c9350829d8fab98454ac39a5b7
aisyah <saisyah0704@gmail.com>
2025-11-25 14:32:22 +0700

    qwerty

--- src/swagger/swagger.tags.ts
+++ src/swagger/swagger.tags.ts
@@ -5,13 +5,13 @@ export const SwaggerTagDescriptions = [
   },
   {
     name: 'Acos',
-    description: 'Endpoint untuk data ACOS dan proses sinkronisasi.',
+    description: 'Endpoint untuk sebuah data ACOS dan proses sinkronisasi.',
   },
   {
     name: 'TypeAkuntansi',
     description: `Ini Endpoint Type Akuntansi, id type akuntansi (type_id) ada dipake 
     ditempat lain (lupa dimana) jadi kalo mau hapus ddskanksjdnksjdnsjkanjkdnfj
-    kandankdnknfd qqqqqqqqqqqqqqqqqqqqqqqqqqqqq
+    kandankdnknfd aaaaaaaaaaaaaaaaaaaaa
     dlfamdmfknnnnnkjnfajdjabdjbjabdjabjdbmfacm kjavanakhdkagn
     adklndklafoiehfnjkadnvkajdbfkjdkdnf 
     asjdkafnknfdnfjdfjkdfjfjdbjdbfiuahefnwaefjdfjabi cek dulu dia dipake atau engga`
